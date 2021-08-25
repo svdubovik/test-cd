f1b4e18448fc55fc2d568847ac7a125f6f8dc233
Svyatoslav Dubovik <svdubovik@gmail.com>
2021-08-25 17:10:57 +0300

    feat: add couple of Vulnerabilities

--- hello.go
+++ hello.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
 
-func Hello() string {
-	return "Hello, world"
+func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	name := "John"
+
+	tmpl.Execute(w, name)
+
+}
+
+func buildSql(email string) string {
+	return fmt.Sprintf("SELECT * FROM users WHERE email='%s';", email)
 }
 
 func main() {
-	fmt.Println(Hello())
+	buildSql("test@test.com")
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
