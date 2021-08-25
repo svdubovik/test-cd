b9475df638389203663bf72dc1cb4015159c0bc4
Svyatoslav Dubovik <svdubovik@gmail.com>
2021-08-25 17:27:14 +0300

    add test

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := buildSql("test@test.com")
+	want := "SELECT * FROM users WHERE email='test@test.com' AND Password='123456';"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
